8f1fe09c84a410c710a068d9ea3e2c64e88ec2d1
blacksun <18982821376@sina.cn>
2016-07-27 17:22:14 +0800

    项目目录

---
+++ App/Home/Controller/IndexController.class.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Administrator
+ * Date: 2016/7/27 0027
+ * Time: 10:27
+ */
+namespace App\Home\Controller;
+
+class IndexController{
+    public function index(){
+        echo "haha";
+    }
+}
